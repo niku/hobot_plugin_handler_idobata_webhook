cfc3ba0c071ca07e30f5e704fb72e315ee0d772c
niku <niku@niku.name>
2017-09-07 21:28:41 +0900

    Implement

--- lib/hobot/plugin/handler/idobata/webhook.ex
+++ lib/hobot/plugin/handler/idobata/webhook.ex
@@ -3,16 +3,39 @@ defmodule Hobot.Plugin.Handler.Idobata.Webhook do
   Documentation for Hobot.Plugin.Handler.Idobata.Webhook.
   """
 
-  @doc """
-  Hello world.
+  use GenServer
 
-  ## Examples
+  def init({context, config} = args) do
+    for {topic, _builder} <- config, do: apply(context.subscribe, [topic])
+    {:ok, args}
+  end
 
-      iex> Hobot.Plugin.Handler.Idobata.Webhook.hello
-      :world
+  def handle_cast({:broadcast, topic, _ref, data}, {_context, config} = state) do
+    url =
+      Map.fetch!(config, topic)
+      |> Map.fetch!(:url)
+    url = case url do
+            u when is_binary(u) ->
+              String.to_charlist(u)
+            u when is_list(u) ->
+              u
+          end
+    :httpc.request(
+      :post,
+      {
+        url,
+        [],
+        'application/x-www-form-urlencoded',
+        URI.encode_query(source: data, format: :html),
+      },
+      [],
+      []
+    )
+    {:noreply, state}
+  end
 
-  """
-  def hello do
-    :world
+  def terminate(reason, {context, config}) do
+    for {topic, _builder} <- config, do: apply(context.unsubscribe, [topic])
+    reason
   end
 end
--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule Hobot.Plugin.Handler.Idobata.Webhook.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :inets]
     ]
   end
 
